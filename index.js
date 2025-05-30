68ae07edf445c3dcdca26585c63d0c7043ddf2c6
Hao Phan <rinnphan01@gmail.com>
2025-05-29 22:17:47 -0700

    update bt4

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ document.getElementById("bt3_button").onclick = function () {
     const userInput = Number(document.getElementById("bt3").value);
     document.getElementById("result3").innerHTML = `Số tiền Việt là ${new Intl.NumberFormat('vn-VN').format(userInput)}vnd.`
 }
+
+document.getElementById("bt4_button").onclick = function () {
+    
+    const length = Number(document.getElementById("length").value);
+    const width = Number(document.getElementById("width").value);
+
+    if (length < 0 || width < 0) {
+        document.getElementById("result4").innerHTML = `Vui lòng bỏ không bỏ chiều dài hoặc rộng nhở hơn 0.`
+
+    }
+    let chuVi = 0;
+    let dienTich = length * width;
+    if (length == width) {
+        chuVi = length * 4;
+    } else {
+        chuVi = (length + width) * 2;
+    }
+    document.getElementById("result4").innerHTML = `Diện tích là ${dienTich}, Chu vi là ${chuVi}.`
+}
