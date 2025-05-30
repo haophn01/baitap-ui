0e71de61259fa62bf5e43111b02f33beca67a462
Hao Phan <rinnphan01@gmail.com>
2025-05-29 20:06:19 -0700

    Update bai tap 2

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ document.getElementById("submit").onclick = function() {
     let total = days * rate;
     document.getElementById("result").innerHTML = `Tổng số lương của bạn là ${total}vnđ.`;
 }
+
+document.getElementById("submit2").onclick = function() {
+    const mot = Number(document.getElementById("1").value);
+    const hai = Number(document.getElementById("2").value);
+    const ba = Number(document.getElementById("3").value);
+    const bon = Number(document.getElementById("4").value);
+    const nam = Number(document.getElementById("5").value);
+    let total = (mot + hai + ba + bon + nam) / 5;
+    document.getElementById("result2").innerHTML = `Số trung bình là ${total}.`;
+}
